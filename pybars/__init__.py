5d01eac0be097a34470ce8342f17a609c31ef006
Mjumbe Poe <mjumbewu@gmail.com>
2014-06-13 14:04:03 -0400

    0.1.0-mjumbewu-3

--- pybars/__init__.py
+++ pybars/__init__.py
@@ -97,7 +97,7 @@ Typical usage:
 # established at this point, and setup.py will use a version of next-$(revno).
 # If the releaselevel is 'final', then the tarball will be major.minor.micro.
 # Otherwise it is major.minor.micro~$(revno).
-__version__ = (0, 1, 0, 'mjumbewu', 2)
+__version__ = (0, 1, 0, 'mjumbewu', 3)
 
 __all__ = [
     'Compiler',
